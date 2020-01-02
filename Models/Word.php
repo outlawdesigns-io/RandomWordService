9db0d484f292362115e64ae7ad2a6ad344f55645
John Watson <outlawstar4761@gmail.com>
2020-01-02 16:03:27 -0600

    errors

--- Models/Word.php
+++ Models/Word.php
@@ -4,18 +4,19 @@ require_once __DIR__ . '/../Libs/Record/Record.php';
 
 class Word extends Record{
 
+    const DB = 'utility';
     const TABLE = 'english_words';
     const PRIMARYKEY = 'id';
 
     public $word;
 
     public function __construct($UID = null){
-        parent::__construct(self::TABLE,$UID);
+        parent::__construct(self::DB,self::TABLE,self::PRIMARYKEY,$Id);
     }
     public static function getAllIds(){
         $data = array();
         $results = $GLOBALS['db']
-            ->database(parent::DB)
+            ->database(self::DB)
             ->table(self::TABLE)
             ->select(self::PRIMARYKEY)
             ->get();
@@ -27,7 +28,7 @@ class Word extends Record{
     public static function getWordCount(){
       $data = 0;
       $results = $GLOBALS['db']
-          ->database(parent::DB)
+          ->database(self::DB)
           ->table(self::TABLE)
           ->select("count(*) as word_count")
           ->get();
